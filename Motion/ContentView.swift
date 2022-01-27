fac08a26d07e10a252461f8a17618ae95f1dd8bf
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-27 09:59:35 +0800

    Initial Commit

---
+++ Motion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Motion
+//
+//  Created by Giovanni Gaffé on 2022/1/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Motion/MotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionApp.swift
+//  Motion
+//
+//  Created by Giovanni Gaffé on 2022/1/27.
+//
+
+import SwiftUI
+
+@main
+struct MotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
