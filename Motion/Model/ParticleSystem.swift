2b73a8ab0496b546b6bcdf0b3e70b9dd6ad22f7f
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-27 10:29:58 +0800

    set some hue and adjust when looping

--- Motion/Model/ParticleSystem.swift
+++ Motion/Model/ParticleSystem.swift
@@ -11,9 +11,20 @@ class ParticleSystem {
     let image = Image("spark")
     var particles = Set<Particle>()
     var center = UnitPoint.center
+    var hue = 0.0
     
     func update(date: TimeInterval) {
-        let newParticle = Particle(x: center.x, y: center.y)
+        let deathDate = date - 1
+        
+        for particle in particles {
+            if particle.creationDate < deathDate {
+                particles.remove(particle)
+            }
+        }
+        
+        let newParticle = Particle(x: center.x, y: center.y, hue: hue)
         particles.insert(newParticle)
+        hue += 0.01
+        if hue > 1 { hue -= 1}
     }
 }
